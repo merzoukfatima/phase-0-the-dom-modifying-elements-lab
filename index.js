1dbb75173548dfc39da727efa9a4c268cb2c9872
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-08-11 00:32:51 +0100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+//1
+const main=document.querySelector("#main")
+main.remove()
+//2
+const newHeader=document.createElement("h1")
+//3
+newHeader.setAttribute("id","victory")
+//4
+newHeader.textContent="Fatima is the champion"
